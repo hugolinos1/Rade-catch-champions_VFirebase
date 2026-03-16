e5353165af0ce1d37f64e253076ec605153a3784
Hugues R <hugues.rabier@gmail.com>
2026-03-16 15:41:30 +0000

    voilà le nom : rade-catch-storage-7eded

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:778213078997:web:c69d7fb6d2171371b02f5c",
   "apiKey": "AIzaSyDLJGknmBj9cOpuI55I__l3sDw2kvulces",
   "authDomain": "studio-1011029963-7eded.firebaseapp.com",
-  "storageBucket": "studio-1011029963-7eded.firebasestorage.app",
+  "storageBucket": "rade-catch-storage-7eded",
   "measurementId": "",
   "messagingSenderId": "778213078997"
 };
